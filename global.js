9b6d63c701cae2cf9d3af7d873112e3bad941ba0
Anna Rosenbaum <anrosenbaum@ucsd.edu>
2026-04-17 08:54:41 -0700

    add dark/light mode

--- global.js
+++ global.js
@@ -40,3 +40,28 @@ for (let p of pages) {
 
   nav.append(a);
 }
+
+// Dark mode switcher
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `<label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>`,
+);
+
+let select = document.querySelector('.color-scheme select');
+
+if ("colorScheme" in localStorage) {
+  document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+  select.value = localStorage.colorScheme;
+}
+
+select.addEventListener('input', function (event) {
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
