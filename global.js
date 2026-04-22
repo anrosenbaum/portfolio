294e2a9815128ee6c95d07e8168240b1ece9a6fc
Anna Rosenbaum <anrosenbaum@ucsd.edu>
2026-04-22 08:04:22 -0700

    profile and project stats

--- global.js
+++ global.js
@@ -105,4 +105,6 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     `;
     containerElement.appendChild(article);
   }
+}export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
 }
