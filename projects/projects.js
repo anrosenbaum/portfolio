14720066df567eba1823fb79f8f170dd4debce0d
Anna Rosenbaum <anrosenbaum@ucsd.edu>
2026-04-30 08:55:05 -0700

    step 1 lab 5

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
@@ -9,3 +10,24 @@ const projectsTitle = document.querySelector('.projects-title');
 if (projectsTitle) {
   projectsTitle.textContent = `Projects (${projects.length})`;
 }
+
+// Pie chart
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year
+);
+
+let sliceGenerator = d3.pie().value((d) => d[1]);
+let arcData = sliceGenerator(rolledData);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc, idx) => {
+  d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors(idx));
+});
